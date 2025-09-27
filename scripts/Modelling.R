58cfde9a8362f8afb05c394064b7f24493c47103
s2490369 <s2490369@ed.ac.uk>
2025-09-27 20:21:30 +0100

    averaged out deprivation and showed on simple plot

--- scripts/Modelling.R
+++ scripts/Modelling.R
@@ -2,3 +2,22 @@ library(tidyverse)
 library(ggplot2)
 dataset <- read_csv("data/processed/cleaned_data.csv")
 show(dataset)
+
+dataset$KM36 <- as.numeric(dataset$`Kaplan-Meier survival 36m`)
+
+
+dataset[dep_cols] <- lapply(dataset[dep_cols], function(x) as.numeric(as.character(x)))
+
+
+dep_matrix <- as.matrix(dataset[dep_cols])
+
+
+dep_scores <- 1:5
+dataset$Dep_Avg <- apply(dep_matrix, 1, function(x) {
+  sum(x * dep_scores, na.rm = TRUE) / sum(x, na.rm = TRUE)
+})
+
+ggplot(dataset,aes(x=Dep_Avg,y=KM36)) +
+  geom_point()
+
+
