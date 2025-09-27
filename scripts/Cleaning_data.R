b1750f772ebf80f1f4d0d226011ab957342d2931
s2490369 <s2490369@ed.ac.uk>
2025-09-27 19:16:16 +0100

    changed cleaning

--- scripts/Cleaning_data.R
+++ scripts/Cleaning_data.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 raw <- read_csv("data/raw/GDO_data_wide.csv")
 
-cleaned_1 <- raw[,c(1,2,3,15,17,18,21,69,70,71,72,73,74,75,106)]
+cleaned_1 <- raw[,c(1,2,3,15,17,18,21,69,70,71,72,73,74,75,106,198)]
 show(cleaned_1)
 
 cleaned_2 <- cleaned_1[cleaned_1[,1]=="Skin tumours",]
