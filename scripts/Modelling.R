1a1a44cce0074847f100a037fcf0f81c474a693d
s2490369 <s2490369@ed.ac.uk>
2025-09-28 17:42:40 +0100

    Cleaned code and removed redundencies

--- scripts/Modelling.R
+++ scripts/Modelling.R
@@ -1,4 +1,4 @@
-#Define packages - tidyverse contains usefull packages
+#Define packages - tidyverse contains useful packages
 #ggplot2 is a graphing package
 
 library(tidyverse)
@@ -14,57 +14,63 @@ dataset$KM36 <- as.numeric(dataset$'Kaplan-Meier survival 36m')
 #Create a data set of categorical data of Gender
 dataset$Gender <- factor(dataset$Gender, levels = c("Male", "Female"))
 
-
-
-#Creates a vector which contains entries ....
+#Creates a vector which contains column names
 dep_cols <- c("Deprivation quintile 1 - most deprived",
               "Deprivation quintile 2",
               "Deprivation quintile 3",
               "Deprivation quintile 4",
               "Deprivation quintile 5 - least deprived")
 
+#We then create a subset of the data set which now contains the deprivation data numerically
 dataset[dep_cols] <- lapply(dataset[dep_cols], function(x) as.numeric(as.character(x)))
 
-
+#Converts that subset to a matrix for better handling
 dep_matrix <- as.matrix(dataset[dep_cols])
 
-
+#Initialize a numerical scale to represent each level of deprivation
 dep_scores <- 1:5
+
+#We then use a simple function which calculates the mean deprivation for each row and stores it as a new column
 dataset$Dep_Avg <- apply(dep_matrix, 1, function(x) {
   sum(x * dep_scores, na.rm = TRUE) / sum(x, na.rm = TRUE)
 })
 
+#We looked at the plot between average deprivation and survival rate to see what our data looked like this is not part of the modelling
 ggplot(dataset,aes(x=Dep_Avg,y=KM36)) +
   geom_point() +
   geom_smooth(method = "lm", se = TRUE, color = "pink" )+
   theme_minimal()
 
-
-
-
+#Now we need to do a similar thing so we can work with ethnicity, below we are trying to use proportion of white ethnicity as a factor
+#Creates a vector to contain column names
 ethnicity_cols <- c("Ethnicity - White","Ethnicity - Asian excl Chinese","Ethnicity - Black", "Ethnicity - Chinese", "Ethnicity - Mixed", "Ethnicity - Other", "Ethnicity - Unknown")
 
+#Now creating a subset which contains the ethnicity data numerically
 dataset[ethnicity_cols] <- lapply(dataset[ethnicity_cols], function(x) as.numeric(as.character(x)))
 
+#Using the which.max function to find the dominant ethnicity in each group
 get_dominant_ethnicity <- function(row) {
   # replace NAs with -Inf so they are ignored by which.max
   row[is.na(row)] <- -Inf
   ethnicity_cols[which.max(row)]
 }
 
-
+#Now storing the dominant ethnicity in a new column
 dataset$EthnicityFactor <- as.character(apply(dataset[ethnicity_cols], 1, get_dominant_ethnicity))
-
-dataset$EthnicityFactor <- factor(dataset$EthnicityFactor)
-
+#After inspecting this column, dominant ethnicity was either white or unknown, unknown ethnicity often meant no data recorded
+#We therefore filtered out the rows with unknown dominant ethnicity
 dataset <- dataset[dataset[,25]=="Ethnicity - White",]
 
+#Create a new column that stores the total number of people in the group with known ethnicity
 dataset$TotalKnown <- rowSums(dataset[, ethnicity_cols[1:6]], na.rm = TRUE)  # sum all except Unknown
 
-# Compute proportion White (ignoring Unknown)
+#Compute the proportion of White ethnicity in each row and store as a new column (ignoring Unknown)
 dataset$PropWhite <- dataset$`Ethnicity - White` / dataset$TotalKnown
 
+#We have now represented our three factors in a way that can be used in a predictive model for survival rate of each group
+
+#Store and display the results of a multivariable linear regression model
 model <- lm(KM36 ~ Dep_Avg + Gender + PropWhite, data = dataset)
 summary(model)
 
-sigma(model)
+
